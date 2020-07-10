5201f4e74697342d2447c5f9cb6d87463316cc9e
Long Nguyen <longnguyen@Longs-iMac.local>
2020-07-10 01:06:28 -0700

    Finish 5.1 - Dutch flag

--- README.md
+++ README.md
@@ -12,6 +12,11 @@ This repository contains Python solutions implemented by Long Nguyen.
 
     $ python3 <program_name>.py
 
+## Problems completed
+### Arrays
+- [x] 5.1 Dutch national flag
+- [x] 5.2 Adding 1 to an arbitrary integer
+
 ## Tracking your progress
 
 The file [index.html](https://github.com/adnanaziz/EPIJudge/blob/master/index.html) in the root of this project tracks your progress through the problems. Specifically, there's an expanding tab for each chapter. Click on it, and you will see your progress, e.g., as below. This file gets updated each time you execute a program. You can **use this file to map book problems to stub programs**.
--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,9 +9,64 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
+    
+    '''
+    Best approach:
+    Take one clever linear-time pass.
+    '''
+    pivot = A[pivot_index]
+    smaller_than = equal_to = 0
+    larger_than = len(A)
+    while equal_to < larger_than:
+        # Consider all three cases at once.
+        if A[equal_to] == pivot:
+            equal_to += 1
+        elif A[equal_to] < pivot:
+            A[equal_to], A[smaller_than] = A[smaller_than], A[equal_to]
+            # We're now certain of one more smaller element.
+            smaller_than += 1
+            equal_to += 1 
+        else:
+            larger_than -= 1
+            A[larger_than], A[equal_to] = A[equal_to], A[larger_than]
     return
 
+    '''
+    Better approach:
+    Take two linear-time passes.
+    '''
+    # Pointers for efficiency.
+    smaller_than, greater_than = 0, len(A) - 1
+    pivot = A[pivot_index]
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller_than] = A[smaller_than], A[i]
+            smaller_than += 1
+    for j in reversed(range(len(A))):
+        if A[j] > pivot:
+            A[j], A[greater_than] = A[greater_than], A[j]
+            greater_than -= 1
+    return
+
+    '''
+    Naive approach:
+    Find all three categories and modify original array.
+    '''
+    smaller_than = []
+    equal_to = []
+    greater_than = []
+    pivot = A[pivot_index]
+    for num in A:
+        if num < pivot:
+            smaller_than.append(num)
+        elif num == pivot:
+            equal_to.append(num)
+        else:
+            greater_than.append(num)
+    partitioned = smaller_than + equal_to + greater_than
+    for i in range(len(A)):
+        A[i] = partitioned[i]
+    return
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
